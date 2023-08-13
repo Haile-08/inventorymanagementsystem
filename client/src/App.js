56bf8791c36be1dbe50f5032861a5efa6fbbb38d
Haile-08 <haile4cmd@gmail.com>
2023-08-13 22:36:29 +0300

    Setup the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,37 @@
+import { RouterProvider, createBrowserRouter, useRouteError } from 'react-router-dom';
 import './App.css';
+import Home from './components/home/Home';
 
 function App() {
+  function ErrorPage() {
+    const error = useRouteError();
+  
+    return (
+      <div id="error-page">
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </div>
+    );
+  }
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element: <Home />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        }]
+    }
+  ])
   return (
     <div className="App">
       <header className="App-header">
+      <RouterProvider router={router} />
       </header>
     </div>
   );
